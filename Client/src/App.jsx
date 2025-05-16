ede4d64fb48f055ddad6ee0cc18b46362e45dd03
tamimhasan19702 <tamimhasancu@gmail.com>
2025-05-16 20:53:02 +0600

    setting up the frontend part

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // use react-router-dom
+import Home from "./pages/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Home;
