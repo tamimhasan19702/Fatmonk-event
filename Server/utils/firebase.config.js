23b6b1bef4b9d9be1301386309e16f1bf5f59d62
tamimhasan19702 <tamimhasancu@gmail.com>
2025-05-19 21:03:40 +0600

    update firebase.config.js

--- Server/utils/firebase.config.js
+++ Server/utils/firebase.config.js
@@ -7,8 +7,7 @@ var serviceAccount = require("./react-social-1-firebase-adminsdk-e14cd-bf107d493
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket:
-    process.env.FIREBASE_STORAGE_BUCKET || "gs://react-social-1.appspot.com",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 });
 
 const bucket = admin.storage().bucket();
