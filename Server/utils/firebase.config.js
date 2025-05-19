a3c0efbc88a04f78b636a57160be5089afcd7e10
tamimhasan19702 <tamimhasancu@gmail.com>
2025-05-20 01:28:02 +0600

    update .gitignore and firebase.config.js

--- Server/utils/firebase.config.js
+++ Server/utils/firebase.config.js
@@ -1,15 +1,24 @@
 /** @format */
 
-var admin = require("firebase-admin");
+require("dotenv").config();
+const admin = require("firebase-admin");
 
-var serviceAccount = require("../../react-social-1-firebase-adminsdk-e14cd-bde15e595a.json");
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  storageBucket:
-    process.env.FIREBASE_STORAGE_BUCKET || "gs://react-social-1.appspot.com",
 });
 
-const bucket = admin.storage().bucket();
-module.exports = { admin, bucket };
+module.exports = admin;
